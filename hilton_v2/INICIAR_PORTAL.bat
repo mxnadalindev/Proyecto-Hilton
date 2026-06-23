83cb4c8c6839434d729502af7300660ce4cdb664
Max <nadalinmaximilianooscar@gmail.com>
2026-06-23 13:37:28 -0300

    refactor: eliminar carpeta hilton_v2 duplicada

--- hilton_v2/INICIAR_PORTAL.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start /min "" node server.js
-timeout /t 3 /nobreak >nul
-start "" "http://localhost:5000"
--- hilton_v2/src/routes/middleware.js
+++
@@ -1,6 +0,0 @@
-// src/routes/middleware.js
-function loginRequerido(req, res, next) {
-  if (!req.session.usuario) return res.redirect('/login');
-  next();
-}
-module.exports = { loginRequerido };
